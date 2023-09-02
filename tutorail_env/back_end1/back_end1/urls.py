ba56e58d1be744ca1961c08bf050d76fa62af878
tokukiyu <tokkiyuam@gmail.com>
2023-09-02 22:39:53 +0300

    a

--- tutorail_env/back_end1/back_end1/urls.py
+++ tutorail_env/back_end1/back_end1/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),   
     path('', views.home, name='home'),
     
-    path('/homeA', views.homeA, name='homeA'),
+    path('/hom', views.homeA, name='homeA'),
     
 ]
