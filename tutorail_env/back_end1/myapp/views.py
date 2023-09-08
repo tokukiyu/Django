a37cf7d303dc8577b42e6a0c41c66eab1f1b720c
tokukiyu <tokkiyuam@gmail.com>
2023-09-08 10:02:36 +0300

    up

--- tutorail_env/back_end1/myapp/views.py
+++ tutorail_env/back_end1/myapp/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home (re):
-    return HttpResponse('hello world of django')
+    return HttpResponse('hello world of django new')
   
