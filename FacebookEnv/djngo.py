23be4e4387c6126f8be7ecd9c37d59738513040c
tokukiyu <tokkiyuam@gmail.com>
2023-09-08 21:57:42 +0300

    no chnge

--- FacebookEnv/djngo.py
+++ FacebookEnv/djngo.py
@@ -4,10 +4,11 @@ import requests
 
 access_token = 'EAAEiZAG3Kh08BO7tvP4nvj9EqG3uZBZBHeTGkfLWVgkHTDuZC2mSuhPE2yKsO6b0WZAsXvhZBrTGj4HMMFrVv6nvfRTPkX0x7Dy4cEjT2OHBvtZAH3pZA1oOGIYYeF2So9HwIEmMaSMIMn6RvMyKG1ivQ3h0mxHj2poxNBqhaJ3meozxcGrWREPvZAJQLS5WgMgljicJxuzwAOLihsLIQiAYYE7YUaZAoZD' 
 
-# url = f"https://graph.facebook.com/v13.0/search?q=%23oromo&type=post&access_token={access_token}"
-
+# Encode the hashtag
 hashtag_encoded = urllib.parse.quote('#oromo')
-url = f"https://graph.facebook.com/v13.0/search?q={hashtag_encoded}&type=post&access_token={access_token}"
+
+# Construct the API request URL
+url = f"https://graph.facebook.com/v17.0/search?q={hashtag_encoded}&type=post&access_token={access_token}"
 
 # Send the GET request to Facebook's Graph API
 response = requests.get(url)
