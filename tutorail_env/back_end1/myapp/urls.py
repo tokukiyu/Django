bfc4040611fd503cd6f893d4d063811519fc41a0
tokukiyu <tokkiyuam@gmail.com>
2023-09-09 10:51:38 +0300

    url parameter

--- tutorail_env/back_end1/myapp/urls.py
+++ tutorail_env/back_end1/myapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),  
-    path('try/', views.TryMe, name='TryMe')
+    path('', views.index, name='home'), 
+    path('drinks/<str:drink_name>', views.drinks, name="drink_name"),  
+    # path('try/', views.TryMe, name='TryMe')
 ]
--- tutorail_env/back_end1/myapp/views.py
+++ tutorail_env/back_end1/myapp/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
+from django.http import HttpResponse 
+def index(request): 
+    path = request.path 
+    method = request.method 
+    content=''' 
+<center><h2>Testing Django Request Response Objects</h2> 
+<p>Request path : " {}</p> 
+<p>Request Method :{}</p></center> 
+'''.format(path, method) 
+    return HttpResponse(content,content_type='text/html', charset='utf-8') 
 
-# Create your views here.
-from django.http import HttpResponse
-
-def home (re):
-    return HttpResponse('hello world of django new')
-def TryMe(re):
-    return HttpResponse('<h1>hello world of django new </h1')
+def drinks(request, drink_name):
+    drink = {
+        'mocha' : 'type of coffee',
+        'tea' : 'type of hot beverage',
+        'lemonade': 'type of refreshment'
+    }
+    choice_of_drink = drink[drink_name]
+    return HttpResponse(f"<h2>{drink_name}</h2> " + choice_of_drink)
