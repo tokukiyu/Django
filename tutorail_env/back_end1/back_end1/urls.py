6d45127eef681edd87691513f00309011624c145
tokukiyu <tokkiyuam@gmail.com>
2023-09-10 09:58:09 +0300

    u

--- tutorail_env/back_end1/back_end1/urls.py
+++ tutorail_env/back_end1/back_end1/urls.py
@@ -1,6 +1,6 @@
 """
 URL configuration for back_end1 project.
-
+ 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
