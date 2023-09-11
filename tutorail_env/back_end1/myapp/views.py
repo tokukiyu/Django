0bdfa2a76f0df42b075923c6a13ed3124cf79b37
tokukiyu <tokkiyuam@gmail.com>
2023-09-11 09:50:58 +0300

    template worked

--- tutorail_env/back_end1/myapp/views.py
+++ tutorail_env/back_end1/myapp/views.py
@@ -7,7 +7,8 @@ def home(request):
     return HttpResponse("Welcome to Little Lemon !")
 
 def about(request):
-    return render(request, "about.html")
+    about_content={'about': "<p> lorem 4 p random </p>"}
+    return render(request, "about.html", about_content)
 
 def menu(request):
     return HttpResponse("Menu for Little Lemon")
