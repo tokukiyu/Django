ada1fac7c1c02c73ffb1fb36577a25d4d66d19bc
tokukiyu <tokkiyuam@gmail.com>
2023-09-12 10:01:33 +0300

    Update djngo.py

--- FacebookEnv/djngo.py
+++ FacebookEnv/djngo.py
@@ -3,7 +3,7 @@ import facebook
 access_token = "EAAEiZAG3Kh08BOzIgI0ZBOV1NgYTwZAZBvnP01NMpLR8VzCcN2F5df12EWNcfqLbCodU5pj3rXJznkY8wiqh7AjpfHqW8QWp6ZACOXZBGGWpbZAuEvGFEQaBpNFKKgAvj8bFTfKuM68KknEUjgkipzOpxVrZAfjJYoj3fq315v6mSZCgPA1TDM1nrYGflHrYkZAVjU9lZBkQN0UEEZCs75wlDVMZD" 
 
 graph = facebook.GraphAPI(access_token)
-
+ 
 fql_query = ("SELECT post_id, permalink, message FROM post WHERE "
              "hashtags IN ('#oromo') AND is_published = true")
 
