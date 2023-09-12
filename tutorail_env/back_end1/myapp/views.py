338fa0cd1176ce96cbc3ac1fd172e381dfa8834b
tokukiyu <tokkiyuam@gmail.com>
2023-09-12 10:02:54 +0300

    Update views.py

--- tutorail_env/back_end1/myapp/views.py
+++ tutorail_env/back_end1/myapp/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse("Welcome to Little Lemon !")
-
+ 
 def about(request):
     about_content={'about': "<p> lorem 4 p random </p>"}
     return render(request, "about.html", about_content)
